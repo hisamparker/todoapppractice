f4046c9758523fa1d235377fb2d986b55f667ba4
hisamparker <hisamparker@gmail.com>
2021-02-10 09:48:26 +0100

    add new todos function

--- app.js
+++ app.js
@@ -27,3 +27,42 @@ function toggleDone() {
     updateCounters();
 }
 
+function submitTodo() {
+    const inputField = document.getElementById(`new-todo`);
+    const newTodoTitle = inputField.value;
+    
+    createTodo(newTodoTitle);
+
+    inputField.value = null;
+
+    updateCounters();
+}
+
+function createTodo(title) {
+    const listItem = document.createElement('li');
+    listItem.className = 'todo'
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'todo-' + nextTodoId();
+    checkbox.checked = false;
+
+    checkbox.onchange = toggleDone.bind(checkbox);
+    listItem.appendChild(checkbox);
+
+    const space = document.createTextNode(' ');
+    listItem.appendChild(space);
+
+    const label = document.createElement('label');
+    label.htmlFor = checkbox.id;
+    label.innerHTML = title;
+    listItem.appendChild(label);
+
+    const list = document.getElementById('todolist');
+    list.appendChild(listItem);
+}
+
+function nextTodoId() {
+    return document.getElementsByClassName('todo').length + 1;
+}
+
