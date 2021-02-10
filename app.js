48a345a3fe6076ed75110ad84cca860e95b8048b
hisamparker <hisamparker@gmail.com>
2021-02-10 09:07:32 +0100

    updated all todo counters with js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
-const totalCount = document.getElementById(`total-count`);
-      
-const totalToDos = document.getElementsByClassName(`todo`).length;
+function updateCounters() {
+    const totalCount = document.getElementById(`total-count`);
+    const totalToDos = document.getElementsByClassName(`todo`).length;
+    totalCount.innerHTML = totalToDos;
 
-totalCount.innerHTML = totalToDos;
+    const completedCount = document.getElementById(`completed-count`);
+    const completedTodos = document.getElementsByClassName(`completed`).length;
+    completedCount.innerHTML = completedTodos;
+
+    const todoCount = document.getElementById(`todo-count`);
+    const uncompletedTodos = totalToDos - completedTodos;
+    todoCount.innerHTML = uncompletedTodos;
+}
+
+updateCounters()
